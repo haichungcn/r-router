786a4a0dce71a792568080b7636852c7ff510418
haichungcn <haichungcn@gmail.com>
2019-10-30 09:03:01 +0700

    updated home

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,17 +1,39 @@
 import React from "react";
-import {Jumbotron, Button} from 'react-bootstrap';
+import { Jumbotron, Button } from 'react-bootstrap';
 
 export default function HomePage() {
     return (
-        <Jumbotron>
-            <h1>Hello, world! this is the HomePage</h1>
-            <p>
-                This is a simple hero unit, a simple jumbotron-style component for
-                calling extra attention to featured content or information.
-            </p>
-            <p>
-                <Button variant="primary">Learn more</Button>
-            </p>
-        </Jumbotron>
+        <>
+            <Jumbotron>
+                <div className="container">
+                    <h1 className="display-3">Hello, world!</h1>
+                    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                    <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+                </div>
+            </Jumbotron>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+                    </div>
+                    <div className="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+                    </div>
+                    <div className="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                        <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+                    </div>
+                </div>
+
+                <hr />
+
+            </div>
+        </>
     );
 }
