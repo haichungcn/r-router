6333af5815ad2cc6f93549018216227a83019667
haichungcn <haichungcn@gmail.com>
2019-10-30 08:49:07 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { Switch, Route } from "react-router-dom";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from './views/HomePage';
+import Candidates from './views/CandidatesPage';
+import Company from './views/CompanyPage';
+import Nav from './components/Navigation';
+import Candidate from './views/CandidatePage';
+import LoginPage from './views/LoginPage';
+import {useSelector} from 'react-redux';
+import Toast from './components/Toast';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({
+    email:'',
+    password:''
+  });
+
+  const login = useSelector((state)=> state);
+
+  useEffect(() => {
+    setCurrentUser(login);
+  }, [login])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+        <Route path='/company'>
+          <Company />
+        </Route>
+        <Route path='/candidates/:id'>
+          <Candidate />
+        </Route>
+        <Route path='/candidates'>
+          <Candidates />
+        </Route>
+        <Route path='/' exact>
+          {!currentUser.email ? <LoginPage /> : <HomePage />}
+        </Route>
+        <Route path='/'>
+          <h1>404</h1>
+          <h2>You've taken the wrong path</h2>
+        </Route>
+      </Switch>
+      <Toast currentUser={currentUser}/>
     </div>
   );
 }
